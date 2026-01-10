cc4de04199af8c2afa1bc44acddc88a482abfca0
Andrew Bastin <andrewbastin.k@gmail.com>
2026-01-10 19:33:10 +0530

    feat: nix package definiton via naersk

--- flake.nix
+++ flake.nix
@@ -4,14 +4,23 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    naersk = {
+      url = "github:nix-community/naersk";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, naersk }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
+        naersk' = pkgs.callPackage naersk {};
       in
         {
+          packages.default = naersk'.buildPackage {
+            src = ./.;
+          };
+
           devShells.default = pkgs.mkShell {
             packages = with pkgs; [
               cargo
